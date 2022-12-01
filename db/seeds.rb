a815dbd64795437c14bc25cba15e74b8dec61c96
codingvaleria <codingvaleria@gmail.com>
2022-12-01 15:45:26 +0300

    generate seed data and migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+heros = []
+
+8.times do
+   heros << Hero.create(name: Faker::Name.name, super_name: Faker::Superhero.name)
+end
+
+powers = []
+3.times do
+    powers << Power.create(name:Faker::Superhero.power, description: Faker::Lorem.paragraph_by_chars(number: rand(20..45), supplemental: false))
+end
+
+8.times do
+   HeroPower.create(strength: ["Strong", "Weak", "Average"].sample, hero_id: heros.sample.id, power_id:powers.sample.id)
+end
